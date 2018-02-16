df56a41dcec77e95ee6c66b03115dfc7d119cf43
Siripong <siripongpr@yahoo.com>
2018-02-16 10:39:11 +0700

    Update console-weather

--- exercise/console-weather.go
+++ exercise/console-weather.go
@@ -10,18 +10,26 @@ func main() {
 }
 
 func weatherCelsius(number int, description string) (display string) {
-	var numdisplay string
-	numdisplay = fmt.Sprintf("_  _\n_||_\n")
+	var numdisplay1, numdisplay2, numdisplay3 string
 	secondDigit := number%10
-	firstDigit := number - secondDigit
-	fmt.Printf("======%v\n", firstDigit)
-	fmt.Printf("======%v\n", secondDigit)
-	display = fmt.Sprintf("%s\n%v c\n%s\n", numdisplay, number, description)
+	firstDigit := (number - secondDigit) / 10
+	numdisplay1 = numdisplay(firstDigit, 1) + numdisplay(secondDigit, 1)
+	numdisplay2 = numdisplay(firstDigit, 2) + numdisplay(secondDigit, 2)
+	numdisplay3 = numdisplay(firstDigit, 3) + numdisplay(secondDigit, 3)
+	display = fmt.Sprintf("%s\n%s\n%s c\n%v c\n%s\n", numdisplay1, numdisplay2, numdisplay3, number, description)
 	return display
 }
 
 func numdisplay(number int, line int) (display string) {
-	if number == 2 {
+	if number == 1 {
+		if line == 1 {
+			display = "   "
+		} else if line == 2 {
+			display = "  |"
+		} else {
+			display = "  |"
+		}
+	} else if number == 2 {
 		if line == 1 {
 			display = " _ "
 		} else if line == 2 {
@@ -29,6 +37,48 @@ func numdisplay(number int, line int) (display string) {
 		} else {
 			display = "|_ "
 		}
+	} else if number == 3 {
+		if line == 1 {
+			display = " _ "
+		} else if line == 2 {
+			display = " _|"
+		} else {
+			display = " _|"
+		}
+	} else if number == 4 {
+		if line == 1 {
+			display = "   "
+		} else if line == 2 {
+			display = "|_|"
+		} else {
+			display = "  |"
+		}
+	} else if number == 5 {
+		if line == 1 {
+			display = " _ "
+		} else if line == 2 {
+			display = "|_"
+		} else {
+			display = " _|"
+		}
+	} else if number == 7 {
+		if line == 1 {
+			display = " _ "
+		} else if line == 2 {
+			display = "  |"
+		} else {
+			display = "  |"
+		}
+	} else if number == 9 {
+		if line == 1 {
+			display = " _ "
+		} else if line == 2 {
+			display = "|_|"
+		} else {
+			display = " _|"
+		}
+	} else {
+		display = ""
 	}
 	return display
 }
