5a4ced6e27f4af7b12b1b72b6f01a7fce3348023
Siripong <siripongpr@yahoo.com>
2018-02-28 10:55:46 +0700

    Update vending machine

--- exercise/vending-machine.go
+++ exercise/vending-machine.go
@@ -1,49 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/siripongpr/vendingMachine"
+)
 
-type NewVendingMachine struct {
-	t int
-	f int
-	tw int
-	o int
-	softDrink int
-	cannedCoffee int
-	drinkingWater int
-}
-
-func (vm NewVendingMachine) InsertCoin(coin string) {
-	if coin == "T" {
-		vm.t++
-	} else if coin == "F" {
-		vm.f++
-	} else if coin == "TW" {
-		vm.tw++
-	} else if coin == "O" {
-		vm.o++
-	}
-}
-
-func (vm NewVendingMachine) GetInsertedMoney() int {
-	return ((vm.t * 10) + (vm.f * 5) + (vm.tw * 2) + vm.o)
-}
+var coins = map[string]int{"T": 10, "F": 5, "TW": 2, "O": 1}
+var items = map[string]int{"SD": 18, "CC": 12, "DW": 7}
 
 func main() {
-	vm := NewVendingMachine(Coins, Items)
+	vm := vendingMachine.NewVendingMachine(coins, items)
+	fmt.Println("Inserted Money:", vm.InsertedMoney())
 
 	// Buy SD(soft drink) with exact change
 	vm.InsertCoin("T")
 	vm.InsertCoin("F")
 	vm.InsertCoin("TW")
 	vm.InsertCoin("O")
-	fmt.Println("Inserted Money:", vm.GetInsertedMoney())	// 18
+	fmt.Println("Inserted Money:", vm.InsertedMoney())	// 18
 	can := vm.SelectSD()
 	fmt.Println(can) // SD
 
 	// Buy CC(canned coffee) without exact change
 	vm.InsertCoin("T")
 	vm.InsertCoin("T")
-	fmt.Println("Inserted Money:", vm.GetInsertedMoney())	// 20
+	fmt.Println("Inserted Money:", vm.InsertedMoney())	// 20
 	can = vm.SelectCC()
 	fmt.Println(can) // CC, F, TW, O
 
@@ -51,7 +32,7 @@ func main() {
 	vm.InsertCoin("T")
 	vm.InsertCoin("T")
 	vm.InsertCoin("F")
-	fmt.Println("Inserted Money:", vm.GetInsertedMoney())	// 25
+	fmt.Println("Inserted Money:", vm.InsertedMoney())	// 25
 	change := vm.CoinReturn()
 	fmt.Println(change) // T, T, F
 }
